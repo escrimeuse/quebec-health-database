1ffd52e71d0c3516166ee7f18a0112869ca8064a
escrimeuse <cathryn.griffiths@gmail.com>
2026-01-01 21:58:15 -0500

    Update schema, update allowed origins

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This database uses Cloudflare D1 for storage and a Cloudflare worker for the API
 
 Data is pulled from the DonneesQuebec API for the Surgical Waitlist (https://www.donneesquebec.ca/recherche/dataset/chirurgies-portrait-de-la-liste-d-attente). In the future, this DB will also include other health data.
 
+Currently, when deployed the only accepted origin is my personal CF worker domain. If you pull this locally, you can make local requests to the local DB. If you want to deploy it to your own CF account, be sure to update the allowed origins in the worker file.
+
 ## Local Dev
 
 First you'll want to grab the data from DonneesQuebec: 
--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,21 +1,39 @@
+const ALLOWED_ORIGINS = ['https://*.cathryn-griffiths.workers.dev/']
+const ALLOWED_METHODS = ['GET', 'OPTIONS']
+
+const HEADERS = {
+  'Access-Control-Allow-Origin': ALLOWED_ORIGINS.join(', '),
+  'Access-Control-Allow-Methods': ALLOWED_METHODS.join(', '),
+}
+
 export default {
   async fetch(request, env) {
     const { pathname } = new URL(request.url);
+
+    if (!env.IS_LOCAL && !ALLOWED_ORIGINS.includes(request.headers.origin)) {
+      return new Response(undefined, {status: 403})
+    }
+
+    if (!ALLOWED_METHODS.includes(request.method)) {
+      return new Response(undefined, {status: 405})
+    }
 
     if (pathname === "/api/regions") {
       const { results } = await env.health_db
         .prepare("SELECT * FROM regions")
         .run();
-      return Response.json(results);
+
+      return new Response(results, {headers: HEADERS});
     }
 
 	if (pathname === '/api/surgical_specialties') {
 		const {results} = await env.health_db.prepare("SELECT * FROM specialties").run();
-		return Response.json(results);
+		return new Response(results, {headers: HEADERS});
 	}
 
     return new Response(
       "See https://github.com/escrimeuse/quebec-health-database",
+      { headers: HEADERS }
     );
   },
 };
