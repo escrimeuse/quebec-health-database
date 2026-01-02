9a21cfeee5cfa0a8b84ecdabbf2fd0b15e170675
escrimeuse <cathryn.griffiths@gmail.com>
2026-01-01 20:43:13 -0500

    update ts

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -3,7 +3,7 @@
 // Runtime types generated with workerd@1.20251210.0 2025-12-30 
 declare namespace Cloudflare {
 	interface GlobalProps {
-		mainModule: typeof import("./src/index");
+		mainModule: typeof import("./src/worker/index");
 	}
 	interface Env {
 	}
